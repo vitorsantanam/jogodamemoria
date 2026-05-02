83059ac2b1f7d1fb80c41a952fd04357ba87d68f
Vitor Santana <130749055+vitorsantanam@users.noreply.github.com>
2026-05-02 14:11:13 +0000

    feat: atualiza fases do jogo do intruso e ajusta estilo do tabuleiro

--- jogodamemoria/fases-intruso.js
+++ jogodamemoria/fases-intruso.js
@@ -10,28 +10,28 @@
 
 const fasesIntruso = [
   [
-    { nome: 'Banana', imagem: 'imagens/maca.jpg', intruso: false },
-    { nome: 'Maçã', imagem: 'imagens/maca.jpg', intruso: false },
-    { nome: 'Pera', imagem: 'imagens/maca.jpg', intruso: false },
-    { nome: 'Chave', imagem: 'imagens/carro.jpg', intruso: true }
+    { nome: '', imagem: 'imagens/maca.jpg', intruso: false },
+    { nome: '', imagem: 'imagens/maca.jpg', intruso: false },
+    { nome: '', imagem: 'imagens/maca.jpg', intruso: false },
+    { nome: '', imagem: 'imagens/carro.jpg', intruso: true }
   ],
   [
-    { nome: 'Cadeira', imagem: 'imagens/maca.jpg', intruso: false },
-    { nome: 'Mesa', imagem: 'imagens/maca.jpg', intruso: false },
-    { nome: 'Sofá', imagem: 'imagens/maca.jpg', intruso: false },
-    { nome: 'Bola', imagem: 'imagens/carro.png', intruso: true }
+    { nome: '', imagem: 'imagens/maca.jpg', intruso: false },
+    { nome: '', imagem: 'imagens/maca.jpg', intruso: false },
+    { nome: '', imagem: 'imagens/maca.jpg', intruso: false },
+    { nome: '', imagem: 'imagens/carro.png', intruso: true }
   ],
   [
-    { nome: 'Copo', imagem: 'imagens/maca.jpg', intruso: false },
-    { nome: 'Prato', imagem: 'imagens/maca.jpg', intruso: false },
-    { nome: 'Garfo', imagem: 'imagens/maca.jpg', intruso: false },
-    { nome: 'Sapato', imagem: 'imagens/carro.jpg', intruso: true }
+    { nome: '', imagem: 'imagens/maca.jpg', intruso: false },
+    { nome: '', imagem: 'imagens/maca.jpg', intruso: false },
+    { nome: '', imagem: 'imagens/maca.jpg', intruso: false },
+    { nome: '', imagem: 'imagens/carro.jpg', intruso: true }
   ],
   [
-    { nome: 'Carro', imagem: 'imagens/maca.jpg', intruso: false },
-    { nome: 'Moto', imagem: 'imagens/maca.jpg', intruso: false },
-    { nome: 'Bicicleta', imagem: 'imagens/maca.jpg', intruso: false },
-    { nome: 'Livro', imagem: 'imagens/carro.jpg', intruso: true }
+    { nome: '', imagem: 'imagens/maca.jpg', intruso: false },
+    { nome: '', imagem: 'imagens/maca.jpg', intruso: false },
+    { nome: '', imagem: 'imagens/maca.jpg', intruso: false },
+    { nome: '', imagem: 'imagens/carro.jpg', intruso: true }
   ]
 ];
 
